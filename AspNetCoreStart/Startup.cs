69096f7d99d8da91985f097b0b99c6a0f4b21050
Marco Lewandowski <lewandowski@swms.de>
2020-01-07 15:27:52 +0100

    Configuration of https://github.com/Azure-Samples/active-directory-dotnet-native-aspnetcore-v2/tree/master/1.%20Desktop%20app%20calls%20Web%20API

--- AspNetCoreStart/Startup.cs
+++ AspNetCoreStart/Startup.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using AspNetCoreStart.Context;
 using AspNetCoreStart.Messaging;
 using AspNetCoreStart.MultiTenancy;
 using Microsoft.AspNet.OData.Extensions;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -17,14 +13,22 @@ using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Microsoft.AspNetCore.Authentication;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication.OAuth;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json.Linq;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using Microsoft.Identity.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
 
 namespace AspNetCoreStart
 {
@@ -64,6 +68,8 @@ namespace AspNetCoreStart
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("Database")));
 
+            services.AddProtectedWebApi(Configuration);
+
             services.AddMvc(opt =>
             {
                 opt.EnableEndpointRouting = false;
@@ -73,42 +79,13 @@ namespace AspNetCoreStart
                 opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
             });
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.Authority = "https://login.microsoftonline.com/288ad4ba-39f6-4fce-87f5-5b08b5333484/v2.0";
-                options.Audience = "a764cdbb-8a8f-4c09-82c9-27d70dbf51c6";
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    // Clock skew compensates for server time drift.
-                    // We recommend 5 minutes or less:
-                    ClockSkew = TimeSpan.FromMinutes(5),
-                    // Specify the key used to sign the token:
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("this is super secret")),
-                    ValidateIssuerSigningKey = true,
-                    RequireSignedTokens = true,
-                    // Ensure the token hasn't expired:
-                    RequireExpirationTime = true,
-                    ValidateLifetime = true,
-                    // Ensure the token audience matches our audience value (default true):
-                    ValidateAudience = true,
-                    ValidAudience = "a764cdbb-8a8f-4c09-82c9-27d70dbf51c6",
-                    // Ensure the token was issued by a trusted authorization server (default true):
-                    ValidateIssuer = true,
-                    ValidIssuer = "https://login.microsoftonline.com/288ad4ba-39f6-4fce-87f5-5b08b5333484/v2.0"
-                };
-            });
+
 
             services.AddTransient<IDbContextFactory, DbContextFactory>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-           
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
